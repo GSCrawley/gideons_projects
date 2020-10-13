aa72c8badb9a575c41f36bf82f33120642781f90
GSCrawley <gscrawley@gmail.com>
2020-10-13 15:36:40 -0700

    gh pages only showing readme

--- gideons_projects/src/index.js
+++ gideons_projects/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import Express from 'express';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
