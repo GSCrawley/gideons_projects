13d8ec7d0923d062b26e5e31cfe8f454b05aec13
GSCrawley <gscrawley@gmail.com>
2020-10-13 12:48:17 -0700

    for some reason /build was being ignored by git. I commented it out, lets see if that fixes the issue

---
+++ gideons_projects/build/precache-manifest.d2ec9634d2bda5234ec2d57cb115971c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7bc1ec05a1f7f874815c8015e83b82",
+    "url": "/gideons_projects/index.html"
+  },
+  {
+    "revision": "0aa0a469291a4ff50357",
+    "url": "/gideons_projects/static/css/main.4e9e4477.chunk.css"
+  },
+  {
+    "revision": "111ac10244749d852bfd",
+    "url": "/gideons_projects/static/js/2.23df3499.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gideons_projects/static/js/2.23df3499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa0a469291a4ff50357",
+    "url": "/gideons_projects/static/js/main.cb118c70.chunk.js"
+  },
+  {
+    "revision": "b5dde48bca690d2f984b",
+    "url": "/gideons_projects/static/js/runtime-main.596864f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gideons_projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
